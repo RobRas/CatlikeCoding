e1d7aff8852dffde301980bfa26fb21ecfc23419
NAU-STUDENTS\rsr47 <rsr47@nau.edu>
2018-01-25 13:01:55 -0700

    Started object pooling.

---
+++ CatlikeCoding Basics/Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour {
+	[SerializeField] float timeBetweenSpawns = 0.1f;
+	[SerializeField] float velocity;
+	[SerializeField] Stuff[] stuffPrefabs;
+
+	float timeSinceLastSpawn;
+
+	void FixedUpdate() {
+		timeSinceLastSpawn += Time.fixedDeltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn -= timeBetweenSpawns;
+			SpawnStuff();
+		}
+	}
+
+	void SpawnStuff() {
+		Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
+		Stuff spawn = Instantiate<Stuff>(prefab);
+		spawn.transform.localPosition = transform.position;
+		spawn.Body.velocity = transform.up * velocity;
+	}
+}
