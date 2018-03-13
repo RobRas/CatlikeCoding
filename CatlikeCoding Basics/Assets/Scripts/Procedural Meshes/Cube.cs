7861d58ac783c4c15c615eb1a498a2288d4823e4
NAU-STUDENTS\rsr47 <rsr47@nau.edu>
2018-03-13 14:05:37 -0700

    Started procedural cubes.

---
+++ CatlikeCoding Basics/Assets/Scripts/Procedural Meshes/Cube.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	public int xSize, ySize, zSize;
+
+	private Vector3[] vertices;
+	private Mesh mesh;
+
+	private void Awake() {
+		StartCoroutine(Generate());
+	}
+
+	private IEnumerator Generate() {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Procedural Grid";
+		WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+		int cornerVertices = 8;
+		int edgeVectices = (xSize + ySize + zSize - 3) * 4;
+		int faceVectices = (
+			(xSize - 1) * (ySize - 1) +
+			(xSize - 1) * (zSize - 1) +
+			(ySize - 1) * (zSize - 1) * 2);
+
+		vertices = new Vector3[cornerVertices + edgeVectices + faceVectices];
+
+		int v = 0;
+		for (int x = 0; x <= xSize; x++) {
+			vertices[v++] = new Vector3(x, 0, 0);
+			yield return wait;
+		}
+
+		yield return wait;
+	}
+
+	private void OnDrawGizmos() {
+		if (vertices == null) return;
+		Gizmos.color = Color.black;
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.DrawSphere(vertices[i], 0.1f);
+		}
+	}
+}
